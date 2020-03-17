88792ac02a6eaf8e3655b77e81ffce7bb8f1135f
Rich Maiale <richm1123@gmail.com>
2020-03-17 00:43:42 -0400

    3/16 update

--- small_town_teller.py
+++ small_town_teller.py
@@ -1,5 +1,9 @@
+from typing import Dict
+
+
 class Person:
-    def __init__(self, id, first_name, last_name):
+
+    def __init__(self, id: int, first_name, last_name):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
@@ -11,6 +15,8 @@ class Person:
     #     else:
     #         print("Not available")
 
+    def __str__(self):
+        return f"id: {self.id}. owner: {self.first_name} {self.last_name}"
 
 # class myClass(object):
 #     def __init__(self):
@@ -33,51 +39,74 @@ class Person:
 #             for i in self.owner:
 #                 if i not in bank_owners:
 #                     bank_owners.append(i)
-#
-#
+
+
 class Account:
-    def __init__(self, number, acc_type, owner):
-        self.number = number
-        self.type = acc_type
-        self.owner = owner
-#         self.balance = balance
-#
+
+    def __init__(self, number, acc_type, account_owner):
+        self.number: int = number
+        self.type: str = acc_type
+        self.owner: Person = account_owner
+        self.balance: float = 0
+
+    def __str__(self):
+        return f"id: {self.number}. type: {self.type}. balance: {self.balance}"
 #     def unique_number(self):
 #         bank_numbers = []
 #         for i in self.number:
 #             if i not in bank_numbers:
 #                 bank_numbers.append(i)
-#
-#     # number =
-#     # type =
-#     # owner =
-#     # balance =
-#
 
 
 class Bank:
+
     def __init__(self):
-        self.customers = []
-        self.accounts = {}
+        self.customers: list = []
+        self.accounts: dict = {}
 
         # Adding a customer to the bank
-    def add_customer(self, person):
-        self.customers.append(person.id)
+    def add_customer(self, person: Person) -> None:
+        if person.id in self.customers:
+            raise ValueError(f"Customer with id {person.id} already exists.")
+        else:
+            self.customers.append(person.id)
 
-    def add_account(self, account):
+    def add_account(self, account: Account) -> None:
+        if account in self.customers:
+            raise ValueError(f"Customer with id {account.number} already exists.")
+        else:
+            self.accounts[account.number] = account.balance
+            # print('Please try a different type')
+
+    def remove_account(self, account):
         if account in self.accounts:
-            if account not in self.accounts:
-                self.accounts[account.acc_type] = account.acc_type
-                print("Account type:" + account.acc_type)
-            else:
-                print('Please try a different type')
-        # Adding an account to the bank
+            del [self.accounts[account]]
+
+    def deposit(self, account: int, amt: float):
+        if account in self.accounts:
+            self.accounts[account] += amt
+        # self.accounts[acct_num] = amt
+            print('Deposited amount: ' + str(amt))
+        else:
+            raise ValueError(f"Account {account} not valid.")
+
+    def withdraw(self, account: int, amt: float):
+        if amt <= self.accounts[account]:
+            self.accounts[account] -= amt
+        else:
+            print("Insufficient funds")
+
+    def balance_inquiry(self, account: int):
+        if account in self.accounts:
+            print(f"Inquiry result: {self.accounts[account]}")
+
+        # Balance inquiry for a particular self.account
+
+# Adding an account to the bank
         # Removing an
         # account from the bank
         # self.deposit = deposit # Depositing money into an account
         # self.withdrawal = withdrawal # Withdrawing money from an account
-        # self.balance_inquiry = balance_inquiry # Balance inquiry for a particular self.account
-
 
 # Constraints
 # When attempting to register a customer, the customer id must be unique.
@@ -93,16 +122,18 @@ zc_bank = Bank()
 bob = Person(1, "Bob", "Smith")
 #
 zc_bank.add_customer(bob)
-
+print(zc_bank.customers)
 bob_savings = Account(1001, "SAVINGS", bob)
 #
-# zc_bank.balance_inquiry(1001)
+print(zc_bank.accounts)
+zc_bank.balance_inquiry(1001)
 # # # 0
-# zc_bank.deposit(1001, 256.02)
+
 # zc_bank.balance_inquiry(1001)
 # # 256.02
 # zc_bank.withdrawal(1001, 128)
 # zc_bank.balance_inquiry(1001)
-
-zc_bank.add_account(bob_savings)
-zc_bank.add_account(bob_savings)
+#
+# zc_bank.add_account(bob_savings)
+# zc_bank.add_account(bob_savings)
+zc_bank.deposit(1001, 256.02)
